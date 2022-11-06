218ca4473b20b85eb2f7c2317fa7d314379649d5
Nick Baynham <nickbaynham@gmail.com>
2022-11-06 09:03:47 -0500

    added ec2 sdk examples in python

--- python/test/test_ec2.py
+++ python/test/test_ec2.py
@@ -11,3 +11,14 @@ class TestEC2(unittest.TestCase):
         """verify listing EC2 instances"""
         instance_id = "i-0083a3611bac5a24c"
         EC2.list_instances(instance_id)
+
+    def test_create_instance(self):
+        """create a web server and test"""
+
+        # Create an AWS Key Pair
+        EC2.create_keypair()
+
+    def test_create_vps(self):
+        """create a vpc and test"""
+
+        EC2.create_vpc()
